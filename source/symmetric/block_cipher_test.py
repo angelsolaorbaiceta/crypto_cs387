cbdb2b3bb0da23701a59c23ee3d305d84565ea46
Angel <angelsolaorbaiceta@gmail.com>
2021-12-27 16:20:02 +0100

    Test CTR mode decryption

--- source/symmetric/block_cipher_test.py
+++ source/symmetric/block_cipher_test.py
@@ -15,6 +15,8 @@ PLAINTEXT = bytes([0x4C, 0x6F, 0x72, 0x65])
 
 
 class TestCounterMode(TestCase):
+    ciphertext = bytes([0x4E, 0xC, 0x70, 0x5])
+
     def setUp(self):
         self.cipher_executor = CTRMode(
             key=KEY, nonce=NONCE, block_size_bytes=BLOCK_SIZE_BYTES
@@ -22,7 +24,14 @@ class TestCounterMode(TestCase):
 
     def test_encrypt(self):
         cipher_0 = self.cipher_executor.encrypt_block(PLAINTEXT[0:2])
-        self.assertEqual(bytes([0x4E, 0xC]), cipher_0)
+        self.assertEqual(self.ciphertext[0:2], cipher_0)
 
         cipher_1 = self.cipher_executor.encrypt_block(PLAINTEXT[2:4])
-        self.assertEqual(bytes([0x70, 0x5]), cipher_1)
+        self.assertEqual(self.ciphertext[2:4], cipher_1)
+
+    def test_decrypt(self):
+        plain_0 = self.cipher_executor.decrypt_block(self.ciphertext[0:2])
+        self.assertEqual(PLAINTEXT[0:2], plain_0)
+
+        plain_1 = self.cipher_executor.decrypt_block(self.ciphertext[2:4])
+        self.assertEqual(PLAINTEXT[2:4], plain_1)
